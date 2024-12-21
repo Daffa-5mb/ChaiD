b43ec43d99045808b9e73bf156ff5e389c24f81b
Daffa-5mb <Misterhacker081@gmail.com>
2024-12-21 21:07:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function sanitizeHTML(str) {
 sendButton.addEventListener('click', () => {
     const userMessage = userInput.value.trim(); // Hilangkan spasi di awal/akhir
     if (userMessage) {
-        chatbox.innerHTML += `<div class="user-message"><strong>Anda:</strong> <span class="message">${sanitizeHTML(userMessage)}</span></div>`;
+        chatbox.innerHTML += `<div class="user-message"><strong>Kamu:</strong> <span class="message">${sanitizeHTML(userMessage)}</span></div>`;
         userInput.value = '';
 
         // Menampilkan indikator mengetik
